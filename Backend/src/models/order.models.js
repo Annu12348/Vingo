ae939ce6d251596d3dd3ab3b5015159ac8adc752
Annu Singh <annu37752@gmail.com>
2025-11-20 23:01:06 +0530

    check page  started

--- Backend/src/models/order.models.js
+++ Backend/src/models/order.models.js
@@ -1,18 +1,55 @@
+//user, paymentMethod, deliveryAddress=>text,latitude,longitude, totalAmount, shopOder
+//shopOderSchema=shop, owner, subtotal, shopOrderItems
+//item,price,quantityj
 import mongoose from "mongoose"
 
-const orderSchema = new mongoose.Schema({
+const shopItemOrderSchema = new mongoose.Schema({
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item"
+    },
+    price: {
+        type: Number
+    },
+    quantity: {
+        type: Number
+    }
+}, {timestamps: true})
+
+const shopOrderSchema = new mongoose.Schema({
+    shop: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Shop",
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    subTotal: {
+        type: Number
+    },
+    shopItem: [shopItemOrderSchema]
+}, {timestamps: true})
+
+const orderSehema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     },
-    paymentAddress: {
-        
-    }
-});
+    paymentMethod: {
+        type: String,
+        enum: ["case payment", "online payment"]
+    },
+    deliveryAddress: {
+        text: String,
+        latitude: Number,
+        longitude: Number,
+    },
+    totalAmount: {
+        type: Number,
+    },
+    shopOder: [shopOrderSchema]
+}, {timestamps: true})
 
-const orderModel = mongoose.model("order", orderSchema);
+const orderModel = mongoose.model("order", orderSehema);
 export default orderModel;
-
-//user, paymentMethod, deliveryAddress=>text,latitude,longitude, totalAmount, shopOder
-//shopOderSchema=shop, owner, subtotal, shopOrderItems
-//item,price,quantityj
--- Frontend/src/pages/CheckOut.jsx
+++ Frontend/src/pages/CheckOut.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import { MdMyLocation } from "react-icons/md";
 
 const CheckOut = () => {
   return (
-    <div>
-      he
+    <div className="min-h-screen w-full py-[1px] ">
+      <Link to="/cart" className="text-2xl text-zinc-600 mt-2 ml-3 block ">
+        <FaArrowLeftLong />
+      </Link>
+      <div className="w-full flex items-center justify-center p-3 h-[95vh]">
+        <div className="py-4 px-4 shadow-lg w-[40%] bg-white rounded-lg ">
+          <h1 className="text-xl capitalize font-bold tracking-tight leading-none ">
+            checkout
+          </h1>
+          <div className="mt-4.5 flex items-end w-full gap-2">
+            <div className="w-[85%]">
+              <label className="flex items-center gap-2 text-md capitalize font-semibold tracking-tight leading-none ">
+                <span className="text-xl text-red-500">
+                  <FaMapMarkerAlt />
+                </span>
+                delivery location
+              </label>
+              <input
+                className="border-1 w-full mt-1 py-1.5 px-2 capitalize outline-none rounded-md text-zinc-500 font-semibold tracking-tight leading-none"
+                type="text"
+                placeholder="enter your delivery address"
+              />
+            </div>
+            <span className=" bg-red-500 cursor-pointer p-2.5 rounded-lg text-white font-semibold">
+              <CiSearch />
+            </span>
+            <span className=" bg-blue-500 cursor-pointer p-2.5 rounded-lg text-white font-semibold">
+              <MdMyLocation />
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CheckOut
+export default CheckOut;
