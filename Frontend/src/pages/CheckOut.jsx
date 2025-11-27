d0a212ec2034360c052123716394afaa95eeba6b
Annu Singh <annu37752@gmail.com>
2025-11-27 19:22:44 +0530

    ghbj klji

--- Frontend/src/pages/CheckOut.jsx
+++ Frontend/src/pages/CheckOut.jsx
@@ -4,6 +4,9 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import { MdMyLocation } from "react-icons/md";
+import { RiSecurePaymentFill } from "react-icons/ri";
+import { FaMobile } from "react-icons/fa";
+import { MdPayment } from "react-icons/md";
 
 const CheckOut = () => {
   return (
@@ -11,7 +14,7 @@ const CheckOut = () => {
       <Link to="/cart" className="text-2xl text-zinc-600 mt-2 ml-3 block ">
         <FaArrowLeftLong />
       </Link>
-      <div className="w-full flex items-center justify-center p-3 h-[95vh]">
+      <div className="w-full flex items-center justify-center pb-10  p-3 h-[95vh]">
         <div className="py-4 px-4 shadow-lg w-[40%] bg-white rounded-lg ">
           <h1 className="text-xl capitalize font-bold tracking-tight leading-none ">
             checkout
@@ -37,6 +40,66 @@ const CheckOut = () => {
               <MdMyLocation />
             </span>
           </div>
+          <div className="w-full h-[26vh] border mt-5 rounded-lg border-zinc-400 flex items-center justify-center ">
+          <h1>ismein map ka images hoga jo baad mein set karna hai</h1>
+          </div>
+          <div className="w-full flex items-center justify-between mt-2.5  ">
+            <div className="p-2 flex gap-3 items-center mt-3  border border-red-700  w-[49%] rounded ">
+              <span className="text-xl bg-zinc-400 block w-fit p-1.5 rounded-full ">
+                <RiSecurePaymentFill />
+              </span>
+              <div>
+                <h1 className="text-md tracking-tight leading-none font-semibold capitalize ">
+                  payment method
+                </h1>
+                <p className="text-sm text-zinc-400 font-semibold mt-1 ">
+                  Pay when your food arrives
+                </p>
+              </div>
+            </div>
+            <div className="p-2 flex gap-3  items-center shadow bg-zinc-200 mt-3 w-[50%] rounded ">
+              <span className="text-xl bg-zinc-400 block w-fit p-1.5 rounded-full ">
+                <FaMobile />
+              </span>
+              <span className="text-xl bg-zinc-400 block w-fit p-1.5 rounded-full ">
+                <MdPayment />
+              </span>
+              <div>
+                <h1 className="text-md tracking-tight leading-none font-semibold capitalize ">
+                  UPI / Credit / Debit Card
+                </h1>
+                <p className="text-sm text-zinc-400 font-semibold mt-1 ">
+                  Pay securely online
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full  mt-5 ">
+            <h1 className="text-md capitalize font-semibold tracking-tight leading-none">
+              order summary
+            </h1>
+            <div className="py-2 px-3 border mt-2.5 rounded border-zinc-400">
+              <div className="flex items-center justify-between">
+                <h1 className="text-zinc-500 font-semibold capitalize tracking-tight ">corn pizza X 1</h1>
+                <h1 className="font-semibold text-zinc-600">₹199.00</h1>
+              </div>
+              <div className="flex items-center justify-between mt-3.5">
+                <h1 className=" font-semibold capitalize tracking-tight ">subtotal</h1>
+                <h1 className="font-semibold">₹199.00</h1>
+              </div>
+              <div className="flex items-center justify-between mt-1.5">
+                <h1 className=" font-semibold  text-zinc-500 capitalize tracking-tight ">delivery</h1>
+                <h1 className="font-semibold text-zinc-500">₹40.00</h1>
+              </div>
+              <div className="flex items-center justify-between mt-1.5">
+                <h1 className=" font-semibold  text-red-500 capitalize tracking-tight ">total</h1>
+                <h1 className="font-semibold text-red-500">₹239.00</h1>
+              </div>
+            </div>
+          </div>
+
+          <button className="mt-5 bg-red-500 py-3 w-full rounded text-white capitalize font-semibold  ">place order</button>
         </div>
       </div>
     </div>
