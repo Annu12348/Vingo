12868ea649631a150a6b804a90ef92aa18697e21
Annu Singh <annu37752@gmail.com>
2026-02-21 20:51:59 +0530

    vercel1

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -3,8 +3,6 @@ import { config } from "../config/config.js";
 import debug from 'debug';
 const debuglog = debug("development:mongoose")
 
-console.log("MONGODB_URL:", config.MONGODB_URL);
-
 function connectDataBase() {
     mongoose.connect(config.MONGODB_URL)
     .then (() => {
